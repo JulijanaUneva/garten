87c06c0aa4ce0716f4ae6232075633f2da7e91d2
julijana <julijana3uneva@gmail.com>
2026-07-22 09:20:34 +0200

    google analytics

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,65 @@
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata = {
+
+    metadataBase: new URL("https://kd-hausundgarten.de"),
+
+    alternates: {
+     canonical: "/",
+  },
+
   title: "K&D Dienstleistungen",
   description: "Gartenpflege und Dienstleistungen in Hamm",
+
+  openGraph: {
+
+    title: "K&D Dienstleistungen | Gartenpflege in Hamm",
+    description: "K&D Dienstleistungen bietet professionelle Gartenpflege, Entrümpelung, Transporte, Montage, Allround-Service und viele weitere Dienstleistungen in Hamm und Umgebung.",
+    url: "https://kd-hausundgarten.de", 
+    siteName: "K&D Dienstleistungen",
+    images: [
+      {
+        url: "/hero-garten.jpeg",
+        width: 1200,
+        height: 630,
+        alt: "K&D Dienstleistungen",
+      },
+    ],
+    locale: "de_DE",
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "K&D Dienstleistungen",
+    description: "Professionelle Gartenpflege und Dienstleistungen in Hamm.",
+    images: ["/hero-garten.jpeg"],
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="de">
       <body suppressHydrationWarning>
+
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-R772F1EHZS"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-R772F1EHZS');
+          `}
+        </Script>
+
         {children}
+
       </body>
     </html>
   );
